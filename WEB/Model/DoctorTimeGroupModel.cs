f68b924d16ebdc0c12634a072fca740e97e87397
wudi <2269818359@qq.com>
2018-06-20 09:46:44 +0800

    医生行程修改

---
+++ WEB/Model/DoctorTimeGroupModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WEB.Model
+{
+    public class DoctorTimeGroupModel
+    {
+        public string Name { get; set; }
+
+        public List<string> GroupList { get; set; }
+    }
+}
---
+++ WEB/Model/DoctorTimeInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WEB.Model
+{
+    public class DoctorTimeInfo
+    {
+        public string Name { get; set; }
+
+        public DateTime? Time { get; set; }
+    }
+}
